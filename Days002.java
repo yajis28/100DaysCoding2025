08680f685d2f46caaa80b3f761cc13b325f66a7e
yajis28 <yajisjis534@gmail.com>
2025-09-09 16:02:25 +0800

    Create Days002.java

---
+++ Days002.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //batas print
+        System.out.print(" •Day2.java\n\n");
+        
+        //batas println
+        System.out.println("  Assalamualaikum wbr");
+        System.out.println("  Salam satu Hobi");
+        
+        //batas printf
+        System.out.printf("\n  Nama: %s\n  Umur: %d\n","yajis",19);
+    } 
+    }
+    
