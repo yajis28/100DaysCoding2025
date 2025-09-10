342edb80323f50edb6395b17c92b93c59bf0c775
yajis28 <yajisjis534@gmail.com>
2025-09-10 12:16:10 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,24 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //1. println digunakan untuk mencetak teks denganpindah beris setelah mencetak
+        // contoh:
+        System.out.println(" nama:");
+        System.out.println(" yajis");
+        
+        //2. print Digunakan untuk mencetak teks tampa pindah baris
+        //contoh:
+        System.out.print(" perkenalkan nama ");
+        System.out.print("sayah yajis\n");
+        // nah otpunya akan tersambung kesana, peerkenalkan nama sayah yajis
+        
+        //3. printf Digunakan teks dengan format tertentu
+        //contoh:
+        System.out.printf(" Nama: %s, \n Umur: % d\n","yajis",19);
+        // otpunya akan menjadi Nama: yajis
+        //                      Umur: 19 
+    } 
+    }
+    
+    
