7e4b43fe9003c1dde480f93ce37fc253b3532b97
yajis28 <yajisjis534@gmail.com>
2025-09-11 12:53:07 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama\t\t: yajis");
+        System.out.println("Umur\t\t: 19 tahun");
+        System.out.println("Alamat\t\t: Balombong\n");
+        System.out.println("Hobi\t\t: main boala, dan \"Coding\"");
+        System.out.println("Status\t\t: Mahasiswa\\Pelajar Informatika");
+    } 
+    }
