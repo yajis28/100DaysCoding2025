77ea1e05d2fd7d0b6e718ba59577ac0786a9c27d
yajis28 <yajisjis534@gmail.com>
2025-09-12 18:42:26 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        byte A = 8;
+        short berat = 40;
+        int hasil = 100;
+        long bulat = 24648L;
+        
+        System.out.println(A);
+        System.out.println(berat);
+        System.out.println(hasil);
+        System.out.println(bulat);
+        
+        
+    } 
+    }
