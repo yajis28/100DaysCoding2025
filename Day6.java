b64f2d207abba38d75d5fee0d4133793d6edf288
yajis28 <yajisjis534@gmail.com>
2025-09-13 20:51:34 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        float ya = 164.3f;
+        double ji = 40.45;
+        System.out.println("Tinggi badan:" + ya);
+        System.out.println("Berat badan:" + ji);
+        
+    } 
+    }
