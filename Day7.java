453f92fef381ad471dc6043f480c85808c5fd040
yajis28 <yajisjis534@gmail.com>
2025-09-14 19:13:03 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        char gender = 'y'; 
+        char simbol ='!';
+        boolean  apakah_aku_mahasiswa = true;
+        boolean apakah_sayasiswa_smk = false;
+        
+        System.out.println (gender);
+        System.out.println(simbol);
+        System.out.println(apakah_aku_mahasiswa);
+        System.out.println(apakah_sayasiswa_smk);
+        
+        
+    } 
+    }
