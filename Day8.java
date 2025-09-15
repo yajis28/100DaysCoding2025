455f905f82c6ce2852ebc8504277c1c0379dc127
yajis28 <yajisjis534@gmail.com>
2025-09-15 17:33:20 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,21 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String Nama = "yajis";
+        String Kls  = "B";
+        String fakultas = "teknik";
+        String prodi = "informatika";
+        String kampus = "Sniversitas Sulawesi Barat";
+        
+        
+        
+        System.out.println ("Nama: " + Nama);
+        System.out.println("Klas: " + Kls);
+        System.out.println("fakultas: " + fakultas);
+        System.out.println("prodi: " + prodi);
+        System.out.println("kampus: " + kampus);
+        
+        
+    } 
+    }
