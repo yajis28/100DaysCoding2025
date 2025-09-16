d55578e149bd3d3b0db4bb6994d6f6d7a9e6cab9
yajis28 <yajisjis534@gmail.com>
2025-09-16 19:24:22 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,22 @@
+package DeklarasiInisialisasidanUpdateVariabel;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String Nama;
+        int Umur;
+        
+        Nama = "yajis";
+        Umur = 18;
+        
+        System.out.println("Deklarasi Awal");
+        System.out.printf("Nama\t: %s\nUmur\t: %d\n\n", Nama, Umur);
+        
+        Nama = "yajisjis";
+        Umur = 17;
+        System.out.println("Update Variabel");
+        System.out.printf("Nama\t: %s\nUmur\t: %d", Nama, Umur);
+        
+    } 
+    }
