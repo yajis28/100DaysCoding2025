4fedff0481d863511bc06eaf344b25b11ef216d9
yajis28 <yajisjis534@gmail.com>
2025-09-17 20:41:22 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+package Day10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        final  int  Umur  = 18;
+        System.out.println("Nilai konstanta yajis adalah: " + Umur);
+        
+        // yajis = 200; // Ini akan error karena yajis adalah final (konstanta}
+        
+}
+    } 
