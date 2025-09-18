2de03b8069f71c81b83b06f97cc2638729e3cf7d
yajis28 <yajisjis534@gmail.com>
2025-09-18 19:35:31 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print(" 1.masukkan nama: ");
+        String nama = sc.nextLine();
+        
+        System.out.print(" 2.masukkan nim: ");
+        String nim = sc.nextLine();
+        
+        System.out.print(" 3.masukkan prodi: ");
+        String prodi = sc.nextLine();
+        
+        System.out.print(" masukkan kls: ");
+        String kls = sc.nextLine();
+        
+        //input 
+        System.out.println(" Nama: " + nama);
+        System.out.println(" Nim: " + nim);
+        System.out.println(" Prodi: " + prodi);
+        System.out.println(" Kls: " + kls);
+        
+        
+}
+    } 
+    
