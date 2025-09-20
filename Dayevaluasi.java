188aa73c5dde2b2c56d80440a99a5a3f3b7ec0cc
yajis28 <yajisjis534@gmail.com>
2025-09-20 21:45:59 +0800

    Dayevaluasi.java

---
+++ Dayevaluasi.java
@@ -0,0 +1,53 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "budi";
+        byte umur = 21;
+        float tinggi = 170.5f;
+        double berat = 65.4;
+        byte nomorAbsen = 12;
+        short sks = 144;
+        String nim = "D0223033";
+        char jeniskelamin = 'L';
+        boolean statusAktif = true;
+        
+
+           
+
+        // Tampilkan dengan println 
+        System.out.println("=== Output println ===");
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi + " cm");
+        System.out.println("Berat: " + berat + " kg");
+        System.out.println("Nomor Absen: " + nomorAbsen);
+        System.out.println("Jumlah SKS: " + sks);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jenis Kelamin: " + jeniskelamin);
+        System.out.println("Status Aktif: " + statusAktif);
+
+        // Tampilkan dengan print
+        System.out.print("\n=== Output print ===\n");
+        System.out.print("Nama: " + nama + ", ");
+        System.out.print("Umur: " + umur + ", ");
+        System.out.print("Tinggi: " + tinggi + " cm, ");
+        System.out.print("Berat: " + berat + " kg, ");
+        System.out.print("Nomor Absen: " + nomorAbsen + ", ");
+        System.out.print("Jumlah SKS: " + sks + ", ");
+        System.out.print("NIM: " + nim + ", ");
+        System.out.print("Jenis Kelamin: " + jeniskelamin + ", ");
+        System.out.print("Status Aktif: " + statusAktif);
+
+        // Tampilkan dengan printf 
+        System.out.println("\n\n=== Output printf ===");
+        System.out.printf("Nama           : %s%n", nama);
+        System.out.printf("Umur           : %d tahun%n", umur);
+        System.out.printf("Tinggi         : %.1f cm%n", tinggi);
+        System.out.printf("Berat          : %.1f kg%n", berat);
+        System.out.printf("Nomor Absen    : %d%n", nomorAbsen);
+        System.out.printf("Jumlah SKS     : %d%n", sks);
+        System.out.printf("NIM            : %s%n", nim);
+        System.out.printf("Jenis Kelamin  : %c%n", jeniskelamin);
+        System.out.printf("Status Aktif   : %b%n", statusAktif);
+    }
+}
