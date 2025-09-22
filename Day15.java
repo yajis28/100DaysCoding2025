c8c58e7486b2783f8363a737f7e57312c195f086
yajis28 <yajisjis534@gmail.com>
2025-09-22 18:59:13 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        int tambah = 10;
+        int kurang = 5;
+
+        // Penjumlahan
+        int hasilTambah = tambah + kurang;
+        System.out.println("Hasil Penjumlahan: " + hasilTambah);
+
+        // Pengurangan
+        int hasilKurang = tambah  - kurang;
+        System.out.println("Hasil Pengurangan: " + hasilKurang);
+    
+
+    }
+}
