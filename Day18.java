e30b9a3bc54c78675dfdda1d3dfd4f37efffea5a
yajis28 <yajisjis534@gmail.com>
2025-09-25 19:19:10 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 10;          
+        int i = b;            
+        long l = i;           
+        float f = l;        
+        double d = f;        
+
+        char c = 'A';        
+        int kodeAscii = c; 
+
+        System.out.println("byte ke int: " + i);
+        System.out.println("int ke long: " + l);
+        System.out.println("long ke float: " + f);
+        System.out.println("float ke double: " + d);
+        System.out.println("char ke int: " + kodeAscii);
+    }
+          }
