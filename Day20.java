2ed11255be487ff9bd9bd97135da4eb1e5e06a01
yajis28 <yajisjis534@gmail.com>
2025-09-27 23:34:03 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,8 @@
+public class Konversi2 {
+    public static void main(String[] args) {
+        int angka = 50;
+
+        String hasil = angka + ""; // angka otomatis dikonversi ke String
+        System.out.println("Hasil konversi: " + hasil);
+    }
+  }
