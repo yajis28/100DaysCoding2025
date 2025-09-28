6e6fa252353cd3b6151aa2088acdce1adf28f749
yajis28 <yajisjis534@gmail.com>
2025-09-28 21:36:54 +0800

    Day21.java

--- Day21.java
+++ Day21.java
@@ -1,19 +1,23 @@
 public class Main {
+    
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         
         int a = 5;
         int b = 10;
+        int tukar = a;
+        a = b;        
+        b = tukar;   
 
         System.out.println("Sebelum ditukar:");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-        
-        int tukar = a;
-        a = b;        
-        b = tukar;    
-
         System.out.println("\nSetelah ditukar:");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
+        
+        
+        
     }
-    }
+    
+}
