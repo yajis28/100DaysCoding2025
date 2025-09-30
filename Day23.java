b7c96bdb9307eb0e626075f2203f02c1fa1fbd99
yajis28 <yajisjis534@gmail.com>
2025-09-30 19:23:01 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double panjang = 20;
+        double lebar = 5;
+        double luas = panjang * lebar;
+        
+        System.out.println("Luas persegi panjang adalah: " + luas);
+    }
+}
