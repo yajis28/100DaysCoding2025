4778de0121a49e03f597d80327ee94145914f50b
yajis28 <yajisjis534@gmail.com>
2025-10-02 17:01:28 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int num = 10;
+        System.out.println("Nilai awal: " + num);
+        
+        num++;
+        System.out.println("Setelah num++ : " + num);
+
+        ++num;
+        System.out.println("Setelah ++num : " + num);
+
+        num--;  
+        System.out.println("Setelah num-- : " + num);
+
+        --num; 
+        System.out.println("Setelah --num : " + num);
+    }
+  }
